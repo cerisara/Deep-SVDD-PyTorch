2322bb44e6a678868db95a5089816905f4c709c3
xtof <cerisara@loria.fr>
2019-06-19 10:00:47 +0200

    save model after AE training

--- src/main.py
+++ src/main.py
@@ -142,6 +142,7 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
                            weight_decay=cfg.settings['ae_weight_decay'],
                            device=device,
                            n_jobs_dataloader=n_jobs_dataloader)
+        deep_SVDD.save_model("aemod")
 
     # Log training details
     logger.info('Training optimizer: %s' % cfg.settings['optimizer_name'])
